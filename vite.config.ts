2f72361659bc3fae0772632e33b651d54ad325f3
PG <74737305+SparkiePG@users.noreply.github.com>
2025-11-08 03:04:15 +0530

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import webExtension from 'vite-plugin-web-extension';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    webExtension({
+      manifest: 'src/manifest.json',
+      watchFilePaths: ['src/manifest.json'],
+    }),
+  ],
+});
